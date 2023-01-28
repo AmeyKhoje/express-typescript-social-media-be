5980233a1c3dbd9f1137c158d48e2dddeb70cce7
Amey Khoje <amey.khoje@ust.com>
2023-01-28 22:38:05 +0530

    some fixes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import * as cookieParser from 'cookie-parser';
-import {config} from 'dotenv';
+import { config } from 'dotenv';
 import { connect } from 'mongoose';
 import Controller from './interfaces/Controller.interface';
 
@@ -12,33 +12,35 @@ class App {
   constructor(controllers: Controller[]) {
     this.app = express();
 
-    this.connectToDatabase()
-    this.initializeMiddleware()
+    this.connectToDatabase();
+    this.initializeMiddleware();
+    this.initializeControllers(controllers);
   }
 
   public listenApp() {
     this.app.listen(process.env.PORT, () => {
       console.log('App started');
-    })
+    });
   }
 
   public initializeMiddleware() {
-    this.app.use(express.json())
-    this.app.use(cookieParser())
+    this.app.use(express.json());
+    this.app.use(cookieParser());
   }
 
   public initializeControllers(controllers: Controller[]) {
-    controllers.forEach(controller => {
-      this.app.use('/', controller.router)
-    })
+    controllers.forEach((controller) => {
+      this.app.use('/api', controller.router);
+    });
   }
 
   private connectToDatabase() {
     try {
-      connect(`${process.env.DB_URL}/${process.env.DB_NAME}`, {keepAlive: true});
-    }
-    catch (error) {
-      throw new Error('Error while connection')
+      connect(`${process.env.DB_URL}/${process.env.DB_NAME}`, {
+        keepAlive: true,
+      });
+    } catch (error) {
+      throw new Error('Error while connection');
     }
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
 import AuthenticationController from 'services/authentication/Authentication.controller';
 import App from './app';
+import UserController from 'services/user/User.controller';
+import PostController from 'services/post/Post.controller';
+import ReactionController from 'services/reaction/Reaction.controller';
 
-const app = new App([new AuthenticationController()]);
+const app = new App([
+  new AuthenticationController(),
+  new UserController(),
+  new PostController(),
+  new ReactionController(),
+]);
 
 app.listenApp();
